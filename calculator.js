fcc819edc4aedc44db48335dff977d176c76d1a6
gering92 <geringeorge92@gmail.com>
2024-04-10 12:26:33 -0700

    Began initial stages of Calculator.

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, operator, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        if (num2 === 0) {
+            return "DISASTER, you can't divide by 0 :(";
+        } else {
+            return divide(num1, num2);
+        }
+    }
+}
